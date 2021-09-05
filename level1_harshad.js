dece436266020b1e88d1ef96650eca8679596ac6
programmer-heeney <programmer.heeney@gmail.com>
2021-09-05 16:41:21 +0900

    하샤드 수 문제풀이

---
+++ level1_harshad.js
@@ -0,0 +1,11 @@
+function solution(x) {
+    var answer = true;
+    const X = x;
+    let sum = 0;
+    while (x > 0) {
+        sum = sum + x % 10;
+        x = Math.floor(x / 10);
+    }
+    answer = X % sum === 0 ? true : false;
+    return answer;
+}
