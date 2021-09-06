f5c17307af97acd1021aece3ca7d25698cc1a0df
programmer-heeney <programmer.heeney@gmail.com>
2021-09-06 10:59:49 +0900

    짝수와 홀수 문제풀이 추가

--- level1_even_odd.js
+++ level1_even_odd.js
@@ -3,3 +3,12 @@ function solution(num) {
     answer = num % 2 === 0 ? 'Even' : 'Odd';
     return answer;
 }
+
+// 음수의 경우 -2 % 2 는 -0이 나옴
+// js가 falsy 하게 보는 값들
+// -0, 0, Null, NaN, undefined, false, ''
+// function solution(num) {
+//     var answer = '';
+//     answer = num % 2 ? 'Odd' : 'Even';
+//     return answer;
+// }
