eb3238fed265938b3103d242f05f3918413b452e
programmer-heeney <programmer.heeney@gmail.com>
2021-09-09 15:01:07 +0900

    문자열 내 p와 y의 개수 문제풀이

--- level1_string_p_y.js
+++ level1_string_p_y.js
@@ -7,3 +7,10 @@ function solution(s) {
     answer = (pArray.length === yArray.length) ? true : false;
     return answer;
 }
+
+// 가지고 있는 개수가 동일하다는 건 그걸 뺀 나머지의 개수도 동일하다는 뜻
+// function solution(s) {
+//     var answer = true;
+//     answer = s.toLowerCase().split('p').length === s.toLowerCase().split('y').length;
+//     return answer;
+// }
