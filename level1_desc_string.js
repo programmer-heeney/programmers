8bb16053f591cea2977fb8720ece1c8c6c13c492
programmer-heeney <programmer.heeney@gmail.com>
2021-09-09 11:02:58 +0900

    문자열 내림차순으로 배치하기 문제풀이

--- level1_desc_string.js
+++ level1_desc_string.js
@@ -1,6 +1,13 @@
+// split 체이닝
 function solution(s) {
     var answer = '';
-    let str = s.split('');
-    answer = str.sort((a, b) => a < b ? 1 : -1).join('');
+    answer = s.split('').sort((a, b) => a < b ? 1 : -1).join('');
     return answer;
 }
+
+// function solution(s) {
+//     var answer = '';
+//     let str = s.split('');
+//     answer = str.sort((a, b) => a < b ? 1 : -1).join('');
+//     return answer;
+// }
